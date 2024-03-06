016ba4bbcafcdc96c9f1df104d955a676a0ddee1
BenjaminScalari <157407147+BenjaminScalari@users.noreply.github.com>
2024-03-06 20:27:31 +0100

    rella sbaga

--- script.js
+++ script.js
@@ -138,6 +138,8 @@ const domande = document.getElementById("question-container")
 const risposte = document.getElementById("answers")
 const conteggioDomande = document.getElementById("conteggioDomande")
 
+let risposteSbagliate = 0;
+let risposteGiuste = 0;
 
 function posizionamentoDomande() {
   let indiceDomanda = 0;
@@ -159,11 +161,47 @@ function posizionamentoDomande() {
         button.innerHTML = risposta;
         button.addEventListener("click", function () {
           mostraDomandaSuccessiva();
+
+          if (risposta[indiceDomanda] === domandaCorrente.correct_answer) {
+            risposteGiuste++;
+            console.log("Risposte Sbagliate: " + risposteGiuste);
+          } else if (risposta[indiceDomanda] === domandaCorrente.correct_answer) {
+            risposteSbagliate++;
+            console.log("Risposte Giuste: " + risposteSbagliate);
+          } else {
+
+          }
         });
         risposteDiv.appendChild(button);
       });
 
       indiceDomanda++;
+
+      // const risposteGiuste = questions[indiceDomanda - 1].correct_answer;
+      // const risposteSbagliate = questions[indiceDomanda - 1].incorrect_answers;
+      // let rispostaSelezionata = risposte;
+    
+      // if (rispostaSelezionata === rispostaGiusta) {
+      //     console.log("Risposte Giuste: " + risposteGiuste);
+      // } else {
+      //   console.log("Risposte Sbagliate: " + risposteSbagliate);
+      // }
+
+      // function conteggioRisposte() {
+      //   if (risposte === questions[indiceDomanda].correct_answer) {
+      //     risposteGiuste += 1;
+      //   } else if (risposte === questions[indiceDomanda].incorrect_answers) {
+      //     risposteSbagliate += 1;
+      //   }
+
+      //   return risposteGiuste;
+      //   return risposteSbagliate;
+      // }
+
+      // conteggioRisposte();
+      // console.log("Risposte Giuste: " + risposteGiuste);
+      // console.log("Risposte Sbagliate: " + risposteSbagliate);
+
       conteggioDomande.innerHTML = "QUESTION " + indiceDomanda
     } else {
       domande.innerHTML = "Fine delle domande";
@@ -181,3 +219,4 @@ function shuffleArray(array) {
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
+
