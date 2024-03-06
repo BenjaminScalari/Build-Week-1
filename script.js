5ee15764e0c1b30944c8f3693d0efb3ffa46204c
MattiaPetru <mattiapetru93@gmail.com>
2024-03-06 23:11:17 +0100

    conteggio domande giuste e sbagliate

--- script.js
+++ script.js
@@ -101,16 +101,19 @@ const questions = [
 //funzione timer che dovrà essere aggiornata
 let tempo = 60;
 let timer = document.getElementById('timer');
+let timerIntervallo = null;
 
 function tempoRimanente() {
   if (tempo <= 0) {
     console.log('tempo scaduto');
+    clearInterval(timerIntervallo);
+    mostraDomandaSuccessiva();
     return;
   }
   tempo--;
   timer.textContent = tempo;
 };
-const timerIntervallo = setInterval(tempoRimanente, 1000);
+
 
 
 
@@ -143,11 +146,13 @@ let risposteGiuste = 0;
 
 function posizionamentoDomande() {
   let indiceDomanda = 0;
+  let quizTerminato = false
 
   function mostraDomandaSuccessiva() {
     if (indiceDomanda < questions.length) {
-      let domandaCorrente = questions[indiceDomanda].question;
-      domande.innerHTML = domandaCorrente;
+      let domandaCorrente = questions[indiceDomanda];
+      domande.innerHTML = domandaCorrente.question;
+      conteggioDomande.innerHTML = 'QUESTION ' + (indiceDomanda + 1);
 
       const risposteDiv = document.getElementById("answers");
       risposteDiv.innerHTML = ""; // Pulisce le risposte precedenti
@@ -160,59 +165,50 @@ function posizionamentoDomande() {
         button.className = "tastoRisposta";
         button.innerHTML = risposta;
         button.addEventListener("click", function () {
-          mostraDomandaSuccessiva();
+          if (quizTerminato) {
+            console.log("Il quiz è terminato")
+            return
+          }
 
-          if (risposta[indiceDomanda] === domandaCorrente.correct_answer) {
+          if (risposta === domandaCorrente.correct_answer) {
             risposteGiuste++;
-            console.log("Risposte Sbagliate: " + risposteGiuste);
-          } else if (risposta[indiceDomanda] === domandaCorrente.correct_answer) {
+            console.log("Risposte Giuste: " + risposteGiuste);
+          } else {
             risposteSbagliate++;
-            console.log("Risposte Giuste: " + risposteSbagliate);
+            console.log("Risposte Sbagliate: " + risposteSbagliate);
+          }
+          if (indiceDomanda === questions.length - 1) {//se arriviamo all'ultima domanda
+            console.log("Ultima domanda raggiunta. Risposte giuste: " + risposteGiuste + ", Risposte sbagliate: " + risposteSbagliate);
+            quizTerminato = true;
+            clearInterval(timerIntervallo); // Ferma il timer
           } else {
 
+            indiceDomanda++
+
+            mostraDomandaSuccessiva();
           }
+
+
         });
         risposteDiv.appendChild(button);
       });
-
-      indiceDomanda++;
-
-      // const risposteGiuste = questions[indiceDomanda - 1].correct_answer;
-      // const risposteSbagliate = questions[indiceDomanda - 1].incorrect_answers;
-      // let rispostaSelezionata = risposte;
-    
-      // if (rispostaSelezionata === rispostaGiusta) {
-      //     console.log("Risposte Giuste: " + risposteGiuste);
-      // } else {
-      //   console.log("Risposte Sbagliate: " + risposteSbagliate);
-      // }
-
-      // function conteggioRisposte() {
-      //   if (risposte === questions[indiceDomanda].correct_answer) {
-      //     risposteGiuste += 1;
-      //   } else if (risposte === questions[indiceDomanda].incorrect_answers) {
-      //     risposteSbagliate += 1;
-      //   }
-
-      //   return risposteGiuste;
-      //   return risposteSbagliate;
-      // }
-
-      // conteggioRisposte();
-      // console.log("Risposte Giuste: " + risposteGiuste);
-      // console.log("Risposte Sbagliate: " + risposteSbagliate);
-
-      conteggioDomande.innerHTML = "QUESTION " + indiceDomanda
-    } else {
-      domande.innerHTML = "Fine delle domande";
     }
   }
-
   mostraDomandaSuccessiva();
+
+  //sicuramente da cambiare 
+  setInterval(function () {
+    mostraDomandaSuccessiva();
+    tempo = 60; // Resetta il timer
+    clearInterval(timerIntervallo);
+    timerIntervallo = setInterval(tempoRimanente, 1000); // Riavvia il timer
+  }, 45000);
 }
 
 posizionamentoDomande();
 
+
+//funzione per randomizzare risposte
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
